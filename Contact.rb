9ef3139f2bf4ec9debd9a3a9100e6ff0bb6a0114
Michael van Holst <mikevanholst@gmail.com>
2013-08-22 14:17:01 -0400

    adds Database class that didn't test well

--- Contact.rb
+++ Contact.rb
@@ -2,35 +2,35 @@ puts "found contacts file"
 
 class Contact
 
-def initialize[first_name, last_name, email, note]
-    @first_name = first_name
-    @last_name = last_name
-    @email = email
-    @note = note
-  end
+	def initialize(first_name, last_name, email, note)
+	    @first_name = first_name
+	    @last_name = last_name
+	    @email = email
+	    @note = note
+	  end
 
-def add_new_contact
+	def add_new_contact
 
-	print "Enter the Clients First Name: "
-	first_name = gets.chomp
-	
-	print "Enter Last Name: "
-  	last_name = gets.chomp
-  	
-  	print "Enter Client Email Address: "
-  	email = gets.chomp
-  	
-  	print "Enter a Note: "
-  	note = gets.chomp
-end
+		print "Enter the Clients First Name: "
+		first_name = gets.chomp
+		
+		print "Enter Last Name: "
+	  	last_name = gets.chomp
+	  	
+	  	print "Enter Client Email Address: "
+	  	email = gets.chomp
+	  	
+	  	print "Enter a Note: "
+	  	note = gets.chomp
+	end
 
 end #class
 
 test_contact = Contact.new("Mike","vanTEst","a@b.com", "yay") 
 
-puts test_contact[1]
+puts test_contact
 
-#add_new_contact
+test_contact.add_new_contact
 
 # puts test_contact.first_name
 # puts test_contact.last_name
